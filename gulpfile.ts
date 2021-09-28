b95a793597b4ea33b6f231ee5ffc1f3c7451067d
Brandon Olivares <programmer2188@gmail.com>
2021-09-27 22:04:01 -0400

    Automatically recompile changed TypeScript

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,10 +1,14 @@
 // noinspection JSUnusedGlobalSymbols
 
-import { dest } from 'gulp';
+import { dest, watch } from 'gulp';
 import * as ts from 'gulp-typescript';
 
 const project = ts.createProject('tsconfig.json');
 
 export function compile(): NodeJS.ReadWriteStream {
   return project.src().pipe(project()).pipe(dest('build'));
+}
+
+export function monitor(): NodeJS.EventEmitter {
+  return watch('src/**/*.ts', compile);
 }
